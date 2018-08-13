dab2ca4fb58dbd7d5874966da17377416d8463e5
Cory Lashway <cory.lashway@gmail.comc>
2018-08-13 09:50:17 -0400

    Udpates

--- src/components/ComparePlayers/ComparePlayers.js
+++ src/components/ComparePlayers/ComparePlayers.js
@@ -10,13 +10,16 @@ const comparePlayers = (props) => {
   const player2 = props.players[1];
   return (
     <div className={classes.ComparePlayers}>
-      <h2 className={classes.CompareTitle}>{player1.name} vs {player2.name}</h2>
-      <Select className={classes.PlaylistFilter} value={props.playlistFilter} displayEmpty autoWidth onChange={props.changed}>
-        <MenuItem value="">Playlist Filter</MenuItem>
-        <MenuItem value="solo">solo</MenuItem>
-        <MenuItem value="duo">duo</MenuItem>
-        <MenuItem value="squad">squad</MenuItem>
-      </Select>
+      <div className={classes.CompareNav}>
+        <h2 className={classes.CompareTitle}>{player1.name} vs {player2.name}</h2>
+        <Select className={classes.PlaylistFilter} value={props.playlistFilter} displayEmpty onChange={props.changed}>
+          <MenuItem value="">Playlist Filter</MenuItem>
+          <MenuItem value="solo">SOLO</MenuItem>
+          <MenuItem value="duo">DUO</MenuItem>
+          <MenuItem value="squad">SQUAD</MenuItem>
+        </Select>
+      </div>
+
       <Grid container spacing={8} justify="center">
         {props.players.map((p, index) => {
           return <Grid item sm={6} md={props.playlistFilter ? 6 : 12} key={p.name + index}>
